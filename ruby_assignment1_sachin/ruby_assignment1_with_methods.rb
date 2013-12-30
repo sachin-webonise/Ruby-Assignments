00221b7f405cf5defdc8ff4722549df38efcb223
sachin.webonise <sachin.wagh@weboniselab.com>
2013-12-30 21:15:18 +0530

    left with some changes

--- ruby_assignment1_sachin/ruby_assignment1_with_methods.rb
+++ ruby_assignment1_sachin/ruby_assignment1_with_methods.rb
@@ -1,136 +1,114 @@
-$string1 = "RUBY parses a file by looking for <br/> one of the special tags that tells it to start interpreting the text as RUBY code. The parser then executes all of the code it finds until it runs into a RUBY closing <br/> tag"
-$string2 = "RUBY does not require (or support) explicit type definition in variable declaration; a variable's type is determined by the context in which the variable is used."
-
-class Assignment
+$string1 = "RUBY parses a file by looking for <br/> one of the special tags \ 
+that tells it to start interpreting the text as RUBY code. The parser then \ 
+executes all of the code it finds until it runs into a RUBY closing <br/> tag"
+$string2 = "RUBY does not require (or support) explicit type definition in \
+variable declaration; a variable's type is determined by the context in which \
+the variable is used."
    
-   def initialize()
-      @array1= $string1.split(" ")
-   end
-   def find_occurance_of_ruby()
-      flag=0 #1st<br/> NOT detected
-      index = 0
-      print "\n\n-------------5) Find occurance of RUBY from string 1. ---------------------\n"
-      while index < @array1.length  do
-         while flag==1 do
-            if @array1[index] == "<br/>"
-              flag=0
-              index +=1
-              break
-            end
-            print @array1[index] 
-            index +=1
-         end
-         if @array1[index] == "<br/>"
-            flag=1#1st<br/> detected
-         end
-         index +=1
-      end
-     print "\n\n" 
-   end#end method
-   def find_position_of_ruby_word
-     print "\n\n-------------6) Find the position where RUBY occures in the string 1.-----------\n\n"
-     index = 0
-     while index < @array1.length  do
-       if @array1[index] == "RUBY"
-         position = index +1
-         puts "\nRUBY occured at position: #{position}" 
-       end
-       index +=1
-     end 
-  end#end method
-  def print_array_using_recursive_function(indexOfWord)
-      if indexOfWord > 0
-         print_array_using_recursive_function(indexOfWord-1)    
-      end 
-    print " #{$arrayOfwordsFromString[indexOfWord]}"    
-  end #end method
-  def capitalise_string_1
-    print "\n\n----------------------------8) Capitalise string 1-----------------------------\n\n"
-    #puts string1.upcase!
-    puts $string1.upcase
-  end #end method
-  def combine_strings
-    print "\n\n----------------------------9) Combine string 1 & 2---------------------------------\n\n"
-    #print "\n\n"
-    $string1.concat($string2)
-    puts $string1
-  end #end method
-  def print_current_date
-    print "\n\n----------------------------10) Print current date------------------------------------\n\n"
-    time = Time.new
-    print "Current Date is: "
-    puts time.strftime("(%Y-%m-%d)\n")
-  end #end method
-  def print_given_date(day,month,year)
-    print "\n\n----------------------------11) print 12th Jan 2012----------------------------------\n\n"
-    passedTime = Time.local(year,month,day)
-    puts passedTime.strftime("(%Y-%m-%d)\n")
-    print passedTime.day
-    case passedTime.day
-        when 1
-          print "st "
-	when 2
-	   print "nd "
-	when 3
-	   print "rd "
-	else
-	   print "th "
-    end#end case
-    case passedTime.month
-	when 1
-	   print "Jan "
-	when 2
-	   print "Feb "
-	when 3
-	   print "Mar "
-	when 4
-	   print "April "
-	when 5
-	   print "May "
-	when 6
-	   print "June "
-	when 7
-	   print "July "
-	when 8
-	   print "Aug "
-	when 9
-	   print "Sept "
-	when 10
-	   print "Oct "
-	when 11
-	   print "Nov "
-	when 12
-           print "Dec "
-    end#end case
-    print passedTime.year
-    print "\n"
-  end #end method
+@array1= $string1.split(" ")
+   
+def find_occurance_of_ruby
+	count_ruby=0
+	@array1.each do |element|
+		if element == "RUBY"
+			count+=1
+		end 
+			count_ruby 
+  end
+end
+puts find_occurance_of_ruby
+
+def find_position_of(word)
+  pos_ruby=1
+	@array1.each do |element|
+	if element == word
+		print "\n#{word} occured at position: #{pos_ruby}"
+	end
+end
+find_position_of("RUBY")
+
+# this method does something.
+def recursive_print(index_of_word)
+	if index_of_word > 0
+    recursive_print(index_of_word-1)    
+  end 
+  print " #{$arrayOfwordsFromString[index_of_word]}"    
+end
+recursive_print()
+
+def capitalise
+  puts $string1.upcase
+end
+capitalise
+
+def combine_strings
+  $string1.concat($string2)
+  puts $string1
+end
+
+def print_current_date
+  time = Time.new
+  print "Current Date is: "
+  puts time.strftime("(%Y-%m-%d)\n")
+end
+print_current_date
+
+def print_given_date(day,month,year)
+  date = Time.local(year,month,day)
+  puts date.strftime("(%Y-%m-%d)\n")
+  print date.day
+  day=date.day
+  if day == 4..20
+    print "st" 
+  else
+    case day % 10   
+ 			when 1
+      	print "st"
+      when 2
+        print "nd" 
+      when 3
+        print "rd"
+      else
+        print "st"
+		end	
+  end
+  months = Hash.new
+  months = {"1" => "Jan", "2" => "Feb", "3" => "Mar", "4" => "Apr",\
+            "5" => "May", "6" => "Jun", "7" => "Jul", "8" => "Aug",\
+            "9" => "Sep", "10" => "Oct", "11" => "Nov", "12" => "Dec"}
+  month = months.day
+  print "#{month}"
+  print passedTime.year
+  print "\n"
+end 
+
   def add_seven_days_to_current_date
-    print "\n\n----------------------------12) add 7 days in current date------------------------------\n\n"
     require 'date'
     today = Date.today
     seven_days_from_now = (today + 7)
     print "After 7 days: #{seven_days_from_now}\n"
-  end #end method
+  end
 
   def cut_string_into_given_no_of_parts(no_of_parts)
         length_of_single_part = $string1.length/no_of_parts
-	puts $string1.length
-	index=0
-	index_for_single_part=0
-	while index<$string1.length do
-	  puts "\nA Part of string: \n"
-	  while index_for_single_part < length_of_single_part do
-	    print $string1[index]
-	    index_for_single_part+=1
-	    index+=1
+	  puts $string1.length
+	  index=0
+	  index_for_single_part=0
+
+	  while index<$string1.length do
+	    puts "\nA Part of string: \n"
+	    while index_for_single_part < length_of_single_part do
+	      print $string1[index]
+	      index_for_single_part+=1
+	      index+=1
+	    end
+	    index_for_single_part=0  
 	  end
-	  index_for_single_part=0  
-	end
-	print "\n\n\n\n"   
-  end #end method
-  def divide_string_and_combine_array
-        print "\n\n--14) Divide the string 1 by occurances of '.'. Combine the array in reverse word sequence----\n\n"
+
+	  print "\n\n\n\n"   
+  end
+
+  def divide_combine        
 	string_saperated_by_occurance_of_dot=$string1.split(".")
 	#puts string_saperated_by_occurance_of_dot
 	words_from_splited_strings=["",""]
@@ -139,7 +117,8 @@ class Assignment
 	index=0
 	index_for_words_array=0
 	while index < string_saperated_by_occurance_of_dot.length do
-	 words_from_splited_strings[index]=string_saperated_by_occurance_of_dot[index].split(" ")
+	 words_from_splited_strings[index]=string_saperated_by_\
+         occurance_of_dot[index].split(" ")
 	 words_from_splited_strings[index].reverse!
 	 index +=1
 
@@ -151,9 +130,10 @@ class Assignment
 	   print words_from_splited_strings[index]
 	   index +=1
 	end
-  end #end method
+  end
+
   def remove_html_characters
-	print "\n\n--------------15) Remove the HTML characters from string.--------------\n\n"
+	
 	array = $string1.split(" ")
 	#print array
 	 
@@ -169,114 +149,106 @@ class Assignment
 	  str[index_for_str] = array[index]
          index +=1 
          index_for_str +=1
-	end# end outer while
+	end
          
-         print "\n--------------After Removing HTML characters------------------------------\n"
+         print "\nAfter Removing HTML characters: \n"
          print str
- end#end method
+ end
+
  def find_string_lengths
-   print "\n\n------------------17) Find the length of string 1 & 2.--------------------\n\n"
    print "\n\nLength of $string1 is: #{$string1.length}"
    print "\nLength of $string2 is: #{$string2.length}\n"
  end
+
  def compare_dates_and_calculate_days(day1,month1,year1,day2,month2,year2)
-   print "\n\n-----18)Compare two dates. Calculate the days between these two dates.----\n\n"
    time_1 = Time.new(year1,month1,day1)
    time_2 = Time.new(year2,month2,day2)
    if time_1.to_date == time_2.to_date
      print "\nDays are Same"
    end
      print "\nNumber of days between given dates are:#{(time_2-time_1)/(24*60*60)}\n"
+ end
 
- end#end method
- def print_date_after_twenty_days
-   print "\n\n-------------19) Print date after 20 days from current date----------------\n\n"
+ def print_future_date
    print "\n Date after 20 days is: "
    today = Date.today
    twenty_days_from_now = (today + 20)
    print " #{twenty_days_from_now}\n" 
- end#end method
+ end
+
  def print_date_in_array_format
-   print "\n\n----------------20) Print date in array format.-----------------------------\n\n"
    time1 = Time.new
    puts "Time in array format : #{["time.day" , "time.month" , "time.year"]}" 
- end#end method
+ end
+
  def regex_for_email_for_weboniselab_domain(email)
-     print "\n\n21) write regex for email for only weboniselab domain\n"
      if email =~ /^[a-z A-Z][\.|\_|\-][a-z A-Z 0-9]* [\@]weboniselab.com/
          print "\n Matching email with pattern\n"
      else
          print "\n NOT Matching email with pattern\n"
      end
-      
- end#end method
+ end
+
  def regex_for_phone_number_pattern(phone_number)
-   print "\n\n22)write regex for phone no format : '+78'followed by 8 nos\n"
-    #/^[\+]78________$/
+     #/^[\+]78________$/
      if phone_number =~ /(^[\+78])(\d[0-9][8])\z/
          print "\n Matching phone number with pattern\n"
      else
          print "\n NOT Matching phone number with pattern\n"
      end
- end#end method
+ end
+
  def regex_to_find_site_and_type(url)
-   print "\n\n23) finding site name and type from a url string using regex\n"
-    
      if url =~ //
          print "\n Matching with pattern\n"
      else
          print "\n NOT Matching with pattern\n"
      end
- end#end method
- def regex_for_given_pattern(given_pattern)
-   print "\n\n24) write regex for this format a.bcdef0000000000000e+05\n"
+ end
 
+ def regex_for_given_pattern(given_pattern)
      if given_pattern =~ //
          print "\n Matching with pattern\n"
      else
          print "\n NOT Matching with pattern\n"
      end
- end#end method
- def regex_for_given_date_format(given_date_format)
-   print "\n\n25)Write Regex for Following date format like 11th Nov 2013 12:34:46\n"
+ end
 
+ def regex_for_given_date_format(given_date_format)
      if given_date_format =~ //
          print "\n Matching given date with pattern\n"
      else
          print "\n NOT Matching given date with pattern\n"
      end
- end#end method
-     
-end#end class
+ end
 
-assignment_object=Assignment.new
-assignment_object.find_occurance_of_ruby
-assignment_object.find_position_of_ruby_word
-print "\n\n---7) Create array of words in string 1 & print them using a recursive function.-----\n\n"
+
+
+
+
 $arrayOfwordsFromString = $string1.split(" ")
 puts "\nno of words from string1 is: #{$arrayOfwordsFromString.length} and words are: "
-assignment_object.print_array_using_recursive_function($arrayOfwordsFromString.length)
 print "\n\n"
-assignment_object.capitalise_string_1
-assignment_object.remove_html_characters
-assignment_object.combine_strings
-assignment_object.print_current_date
-assignment_object.print_given_date(12,1,2012)
-assignment_object.add_seven_days_to_current_date
-print "\n\n------------------13) Cut the string 1 into 4 parts & print it.------------------------\n\n"
-assignment_object.cut_string_into_given_no_of_parts(4)
 
-assignment_object.divide_string_and_combine_array
-assignment_object.remove_html_characters
-assignment_object.find_string_lengths
-assignment_object.compare_dates_and_calculate_days(12,04,2010,12,05,2011)
-assignment_object.print_date_after_twenty_days
-assignment_object.print_date_in_array_format
-assignment_object.regex_for_email_for_weboniselab_domain("sachin.wagh@weboniselab.com")
-assignment_object.regex_for_phone_number_pattern("+7898328732")
-#assignment_object.regex_to_find_site_and_type("http://www.xyz.com/classid/17950142?type=mandatory")
-#assignment_object.regex_for_given_pattern("a.bcdef0000000000000e+05")
-#assignment_object.regex_for_given_date_format("11th Nov 2013 12:34:46")
+remove_html_characters
+combine_strings
+print_current_date
+print_given_date(12,1,2012)
+add_seven_days_to_current_date
+
+cut_string_into_given_no_of_parts(4)
+
+divide_combine
+remove_html_characters
+find_string_lengths
+compare_dates_and_calculate_days(12,04,2010,12,05,2011)
+print_date_after_twenty_days
+print_date_in_array_format
+regex_for_email_for_weboniselab_domain("sachin.wagh@weboniselab.com")
+regex_for_phone_number_pattern("+7898328732")
+#regex_to_find_site_and_type("http://www.xyz.com/classid/17950142?type=mandatory")
+#regex_for_given_pattern("a.bcdef0000000000000e+05")
+#regex_for_given_date_format("11th Nov 2013 12:34:46")
 
 
 
